808414e61075e22a015a9c5c1bc1cbf21768bd1c
Qekly <benjaalejandro206@gmail.com>
2024-01-25 20:07:12 -0300

    chore(home-aggregate): rename old ValueObjectHomePosition objects, perform logic for array usage on creation/deletion for homes, and implement the performHomeDeletion method

--- api/src/main/java/com/aivruu/homes/home/HomeAggregate.java
+++ api/src/main/java/com/aivruu/homes/home/HomeAggregate.java
@@ -2,6 +2,7 @@ package com.aivruu.homes.home;
 
 import com.aivruu.homes.event.HomeCreationEvent;
 import com.aivruu.homes.event.HomeDeleteEvent;
+import com.aivruu.homes.event.HomesCleanEvent;
 import com.aivruu.homes.player.EntityCachedPlayerModel;
 import com.aivruu.homes.repository.PlayerModelRepository;
 import com.aivruu.homes.result.ValueObjectHomeResult;
@@ -10,8 +11,7 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.UUID;
 
 /**
@@ -34,33 +34,35 @@ public class HomeAggregate {
    * @param homeId identifier needed for new home data.
    * @return A status code of {@link ValueObjectHomeResult}, possible status code returned.<p>
    * <p>
-   * • {@link ValueObjectHomeResult#withStatus(Object, byte)} with the {@link ValueObjectHomePosition} and status code {@link ValueObjectHomeResult#ADDED_RESULT}
+   * • {@link ValueObjectHomeResult#withStatus(Object, byte)} with the {@link EntityHomeModel} and status code {@link ValueObjectHomeResult#ADDED_RESULT}
    * for this home if was created correctly.<p>
-   * • {@link ValueObjectHomeResult#withStatus(Object, byte)} with the status code -3 if {@link HomeCreationEvent} instance is cancelled.<p>
-   * • {@link ValueObjectHomeResult#withStatus(Object, byte)} with the status code -2 if player has reached the allowed homes amount limit.<p>
+   * • {@link ValueObjectHomeResult#withStatus(Object, byte)} with the status code -4 if {@link HomeCreationEvent} instance is cancelled.<p>
+   * • {@link ValueObjectHomeResult#withStatus(Object, byte)} with the status code -10 if already exists a home with the same id that was given.<p>
    * • {@link ValueObjectHomeResult#withError()} if player information was not founded.
    * @since 0.0.1
    */
-  public @NotNull ValueObjectHomeResult<@Nullable ValueObjectHomePosition> performHomeCreate(final @NotNull Player player, final @NotNull String homeId) {
+  public @NotNull ValueObjectHomeResult<@Nullable EntityHomeModel> performHomeCreate(final @NotNull Player player, final @NotNull String homeId) {
     final EntityCachedPlayerModel playerModel = this.repository.remove(player.getUniqueId());
     if (playerModel == null) {
       return ValueObjectHomeResult.withError();
     }
-    Map<String, ValueObjectHomePosition> playerHomes = playerModel.homes();
-    if (playerHomes.size() > MAX_ALLOWED_HOMES_AMOUNT) {
-      return ValueObjectHomeResult.withStatus(null, (byte) -2);
+    final EntityHomeModel[] playerHomes = playerModel.homes();
+    for (final EntityHomeModel iteratedHomeModel : playerHomes) {
+      if (!iteratedHomeModel.id().equals(homeId)) {
+        continue;
+      }
+      return ValueObjectHomeResult.withStatus(null, (byte) -10);
     }
-    final ValueObjectHomePosition valueObjectHomePosition = new ValueObjectHomePosition(player.getWorld().getName(), player.getX(), player.getY(), player.getZ());
-    final HomeCreationEvent homeCreationEvent = new HomeCreationEvent(player, homeId, valueObjectHomePosition);
+    final EntityHomeModel entityHomeModel = new EntityHomeModel(homeId, player.getWorld().getName(), player.getX(), player.getY(), player.getZ());
+    final HomeCreationEvent homeCreationEvent = new HomeCreationEvent(player, homeId, entityHomeModel);
     Bukkit.getPluginManager().callEvent(homeCreationEvent);
     if (homeCreationEvent.isCancelled()) {
-      return ValueObjectHomeResult.withStatus(null, (byte) -3);
+      return ValueObjectHomeResult.withStatus(null, (byte) -4);
     }
-    playerHomes.put(homeId, valueObjectHomePosition);
-    this.repository.add(new EntityCachedPlayerModel(player.getUniqueId(), player.getName(), new HashMap<>(playerHomes)));
-    playerHomes.clear();
-    playerHomes = null;
-    return ValueObjectHomeResult.withStatus(valueObjectHomePosition, ValueObjectHomeResult.ADDED_RESULT);
+    final EntityHomeModel[] newHomesArray = Arrays.copyOf(playerHomes, playerHomes.length + 1);
+    newHomesArray[playerHomes.length] = entityHomeModel;
+    this.repository.add(new EntityCachedPlayerModel(player.getUniqueId(), player.getName(), newHomesArray));
+    return ValueObjectHomeResult.withAdded(entityHomeModel);
   }
 
   /**
@@ -75,20 +77,54 @@ public class HomeAggregate {
    * • {@link ValueObjectHomeResult#withError()} if player information was not founded.
    * @since 0.0.1
    */
-  public @NotNull ValueObjectHomeResult<@Nullable ValueObjectHomePosition> performHomeDeletion(final @NotNull UUID id, final @NotNull String homeId) {
+  public @NotNull ValueObjectHomeResult<@Nullable EntityHomeModel> performHomeDeletion(final @NotNull UUID id, final @NotNull String homeId) {
     final EntityCachedPlayerModel playerModel = this.repository.remove(id);
     if (playerModel == null) {
       return ValueObjectHomeResult.withError();
     }
-    Map<String, ValueObjectHomePosition> playerHomes = playerModel.homes();
-    if (playerHomes.remove(homeId) == null) {
+    final EntityHomeModel[] playerHomes = playerModel.homes();
+    boolean founded = false;
+    for (int i = 0 ; i < playerHomes.length ; i++) {
+      if (!playerHomes[i].id().equals(homeId)) {
+        continue;
+      }
+      playerHomes[i] = null;
+      founded = true;
+      break;
+    }
+    if (!founded) {
       return ValueObjectHomeResult.withStatus(null, (byte) -4);
     }
     final HomeDeleteEvent homeDeleteEvent = new HomeDeleteEvent(playerModel.findByUid(), homeId);
     Bukkit.getPluginManager().callEvent(homeDeleteEvent);
-    this.repository.add(new EntityCachedPlayerModel(id, playerModel.name(), new HashMap<>(playerHomes)));
-    playerHomes.clear();
-    playerHomes = null;
+    this.repository.add(new EntityCachedPlayerModel(id, playerModel.name(), Arrays.copyOf(playerHomes, playerHomes.length - 1)));
+    return ValueObjectHomeResult.withRemoved();
+  }
+
+  /**
+   * Removes all the existing homes of the player.
+   *
+   * @param id needed to identify to player data model.
+   * @return A status code of {@link ValueObjectHomeResult} for this operation, expected status codes.<p>
+   * <p>
+   * • {@link ValueObjectHomeResult#withRemoved()} if the homes were deleted correctly.<p>
+   * • {@link ValueObjectHomeResult#withStatus(Object, byte)} with the status code -4 if a {@link HomesCleanEvent}
+   * instance has been cancelled during the operation.<p>
+   * • {@link ValueObjectHomeResult#withError()} if player information was not found.
+   * @since 0.0.1
+   */
+  public @NotNull ValueObjectHomeResult<@Nullable EntityHomeModel> performAllHomesClean(final @NotNull UUID id) {
+    final EntityCachedPlayerModel playerModel = this.repository.remove(id);
+    if (playerModel == null) {
+      return ValueObjectHomeResult.withError();
+    }
+    final EntityHomeModel[] newEmptyHomesArray = new EntityHomeModel[]{};
+    final HomesCleanEvent homesCleanEvent = new HomesCleanEvent(playerModel.findByUid(), newEmptyHomesArray);
+    Bukkit.getPluginManager().callEvent(homesCleanEvent);
+    if (homesCleanEvent.isCancelled()) {
+      return ValueObjectHomeResult.withStatus(null, (byte) -4);
+    }
+    this.repository.add(new EntityCachedPlayerModel(id, playerModel.name(), newEmptyHomesArray));
     return ValueObjectHomeResult.withRemoved();
   }
 }
