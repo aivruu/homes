84aa9c247eff2689914b3074d2f3ba2c4ed45ba6
Qekly <benjaalejandro206@gmail.com>
2024-01-26 14:01:41 -0300

    feat(cloud-storage): add MongoDB storage implementation

---
+++ api/src/main/java/com/aivruu/homes/shared/cloud/MongoDBModelData.java
@@ -0,0 +1,38 @@
+package com.aivruu.homes.shared.cloud;
+
+import com.aivruu.homes.player.EntityCachedPlayerModel;
+import com.aivruu.homes.result.ValueObjectDataResult;
+import com.aivruu.homes.shared.DataModel;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class MongoDBModelData implements DataModel {
+  @Override
+  public @NotNull CompletableFuture<@NotNull Boolean> performLoad() {
+    return null;
+  }
+
+  @Override
+  public @NotNull CompletableFuture<@NotNull Boolean> performUnload() {
+    return null;
+  }
+
+  @Override
+  public @NotNull CompletableFuture<@NotNull ValueObjectDataResult<@Nullable List<@NotNull EntityCachedPlayerModel>>> performAsyncCollectionWrite(final @NotNull Collection<@NotNull EntityCachedPlayerModel> modelsCollection) {
+    return null;
+  }
+
+  @Override
+  public @NotNull CompletableFuture<@NotNull ValueObjectDataResult<@Nullable EntityCachedPlayerModel>> performAsyncWrite(final @NotNull EntityCachedPlayerModel playerModel) {
+    return null;
+  }
+
+  @Override
+  public @NotNull CompletableFuture<@NotNull ValueObjectDataResult<@Nullable EntityCachedPlayerModel>> performAsyncRead(final @NotNull String id) {
+    return null;
+  }
+}
