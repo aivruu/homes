d3083f6b845e48865105ec83e4b80a56137dec10
Qekly <benjaalejandro206@gmail.com>
2024-01-29 21:27:10 -0300

    feat(homes-menu): add configuration model for the player homes menu

---
+++ api/src/main/java/com/aivruu/homes/config/model/HomesMenuConfigModel.java
@@ -0,0 +1,7 @@
+package com.aivruu.homes.config.model;
+
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+@ConfigSerializable
+public class HomesMenuConfigModel extends SerializableConfigModel {
+}
