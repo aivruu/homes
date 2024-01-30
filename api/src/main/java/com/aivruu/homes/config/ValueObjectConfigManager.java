5aa06d7626e19eeebc82861d393a550a076a44a1
Qekly <benjaalejandro206@gmail.com>
2024-01-29 21:31:40 -0300

    chore(config-manager): mark as class and make a single static method for each configuration model load

--- api/src/main/java/com/aivruu/homes/config/ValueObjectConfigManager.java
+++ api/src/main/java/com/aivruu/homes/config/ValueObjectConfigManager.java
@@ -1,8 +1,6 @@
 package com.aivruu.homes.config;
 
-import com.aivruu.homes.config.model.ConfigModel;
-import com.aivruu.homes.config.model.MessageConfigModel;
-import com.aivruu.homes.result.ValueObjectConfigResult;
+import com.aivruu.homes.config.model.SerializableConfigModel;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.configurate.BasicConfigurationNode;
@@ -16,65 +14,34 @@ import java.nio.file.Path;
  *
  * @since 0.0.1
  */
-public enum ValueObjectConfigManager {
-  // We use lazy singleton for access to the reference.
-  INSTANCE;
-
+public class ValueObjectConfigManager {
   /**
-   * Loads and return a {@link ConfigModel}.
+   * Loads and return a serializable config model reference.
    *
-   * @param pluginFolder plugin folder needed to know where create the file.
-   * @return A status code of {@link ValueObjectConfigResult}, possible status codes.<p>
-   * <p>
-   * • {@link ValueObjectConfigResult#withUnload()} if model was not loaded correctly.<p>
-   * • {@link ValueObjectConfigResult#withStatus(Object, byte)} if the model has been loaded.
+   * @param pluginFolder route for file creation.
+   * @param file name for the configuration file.
+   * @param serializableConfigClassType a class that extends of {@link  SerializableConfigModel}.
+   * @return A class that extends of {@link SerializableConfigModel} or {@code null}.
    * @since 0.0.1
    */
-  public @NotNull ValueObjectConfigResult<@Nullable ConfigModel> loadConfig(final @NotNull Path pluginFolder) {
+  public static <T> @Nullable T loadConfig(final @NotNull Path pluginFolder, final @NotNull String file, final @NotNull Class<T> serializableConfigClassType) {
     final GsonConfigurationLoader loader = GsonConfigurationLoader.builder()
       .lenient(true)
       .defaultOptions(opts -> opts.shouldCopyDefaults(true))
-      .path(pluginFolder.resolve("config.json"))
+      .path(pluginFolder.resolve(file))
       .build();
     try {
       final BasicConfigurationNode node = loader.load();
-      final ConfigModel configModel = node.get(ConfigModel.class);
-      node.set(ConfigModel.class, configModel);
-      loader.save(node);
-      assert configModel != null;
-      return ValueObjectConfigResult.withStatus(configModel, ValueObjectConfigResult.LOAD_STATUS);
-    } catch (final ConfigurateException exception) {
-      exception.printStackTrace();
-      return ValueObjectConfigResult.withUnload();
-    }
-  }
+      final T serializedModel = node.get(serializableConfigClassType);
 
-  /**
-   * Loads and return a {@link MessageConfigModel}.
-   *
-   * @param pluginFolder plugin folder needed to know where create the file.
-   * @return A status code of {@link ValueObjectConfigResult}, possible status codes.<p>
-   * <p>
-   * • {@link ValueObjectConfigResult#withUnload()} if model was not loaded correctly.<p>
-   * • {@link ValueObjectConfigResult#withStatus(Object, byte)} if the model has been loaded.
-   * @since 0.0.1
-   */
-  public @NotNull ValueObjectConfigResult<@Nullable MessageConfigModel> loadMessages(final @NotNull Path pluginFolder) {
-    final GsonConfigurationLoader loader = GsonConfigurationLoader.builder()
-      .lenient(true)
-      .defaultOptions(opts -> opts.shouldCopyDefaults(true))
-      .path(pluginFolder.resolve("messages.json"))
-      .build();
-    try {
-      final BasicConfigurationNode node = loader.load();
-      final MessageConfigModel messageConfigModel = node.get(MessageConfigModel.class);
-      node.set(MessageConfigModel.class, messageConfigModel);
+      node.set(serializableConfigClassType, serializedModel);
       loader.save(node);
-      assert messageConfigModel != null;
-      return ValueObjectConfigResult.withStatus(messageConfigModel, ValueObjectConfigResult.LOAD_STATUS);
+      assert serializedModel != null;
+
+      return serializedModel;
     } catch (final ConfigurateException exception) {
       exception.printStackTrace();
-      return ValueObjectConfigResult.withUnload();
+      return null;
     }
   }
 }
