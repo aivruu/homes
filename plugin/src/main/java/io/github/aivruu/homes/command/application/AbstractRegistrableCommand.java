130c2f755d97b023d11550b64fdda8de857829f4
aivruu <benjaalejandro206@gmail.com>
2024-12-22 12:51:06 -0300

    feat(AbstractRegistrableCommand): create abstract-class for commands registering

---
+++ plugin/src/main/java/io/github/aivruu/homes/command/application/AbstractRegistrableCommand.java
@@ -0,0 +1,46 @@
+// This file is part of homes, licensed under the GNU License.
+//
+// Copyright (c) 2024 aivruu
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+package io.github.aivruu.homes.command.application;
+
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.github.aivruu.homes.config.application.ConfigurationInterface;
+import io.github.aivruu.homes.config.infrastructure.object.ConfigurationConfigurationModel;
+import io.github.aivruu.homes.config.infrastructure.object.MessagesConfigurationModel;
+import io.github.aivruu.homes.permission.application.Permissions;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class AbstractRegistrableCommand {
+  protected ConfigurationInterface<ConfigurationConfigurationModel> configuration;
+  protected ConfigurationInterface<MessagesConfigurationModel> messages;
+
+  protected AbstractRegistrableCommand(
+    final @NotNull ConfigurationInterface<ConfigurationConfigurationModel> configuration,
+    final @NotNull ConfigurationInterface<MessagesConfigurationModel> messages
+  ) {
+    this.configuration = configuration;
+    this.messages = messages;
+  }
+
+  @SuppressWarnings("UnstableApiUsage")
+  public abstract @NotNull LiteralCommandNode<CommandSourceStack> register();
+
+  protected final boolean canUseIt(final @NotNull CommandSender sender, final @NotNull Permissions permissionEnum) {
+    return sender.hasPermission(permissionEnum.permission());
+  }
+}
