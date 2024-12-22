09ac61ef1b86a68a9e015033ee8e680e9c5ee399
aivruu <benjaalejandro206@gmail.com>
2024-12-22 12:42:27 -0300

    feat(HomePositionUpdater): create service for homes' location modifications

---
+++ api/src/main/java/io/github/aivruu/homes/home/application/HomePositionUpdater.java
@@ -0,0 +1,89 @@
+// This file is part of homes, licensed under the GNU License.
+//
+// Copyright (c) 2024 aivruu
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+package io.github.aivruu.homes.home.application;
+
+import io.github.aivruu.homes.aggregate.domain.registry.AggregateRootRegistry;
+import io.github.aivruu.homes.aggregate.domain.repository.AggregateRootRepository;
+import io.github.aivruu.homes.home.domain.event.HomePositionUpdateEvent;
+import io.github.aivruu.homes.home.domain.HomeAggregateRoot;
+import io.github.aivruu.homes.home.domain.position.HomePositionValueObject;
+import io.github.aivruu.homes.result.domain.ValueObjectMutationResult;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * This class is used to proportionate the logic for any {@link io.github.aivruu.homes.home.domain.HomeModelEntity}'s
+ * {@link HomePositionValueObject} modification.
+ *
+ * @since 2.0.0
+ */
+public final class HomePositionUpdater {
+  private final AggregateRootRegistry<HomeAggregateRoot> homeAggregateRootRegistry;
+
+  public HomePositionUpdater(final @NotNull AggregateRootRegistry<HomeAggregateRoot> homeAggregateRootRegistry) {
+    this.homeAggregateRootRegistry = homeAggregateRootRegistry;
+  }
+
+  /**
+   * Updates the {@link HomePositionValueObject} of the {@link io.github.aivruu.homes.home.domain.HomeModelEntity} specified
+   * by the given identifier, and return a mutation-result.
+   *
+   * @param player the player who is updating their home's position.
+   * @param homeId the home's id.
+   * @param newLocation the player's {@link Location} used as new home-position.
+   * @return A {@link ValueObjectMutationResult} for this operation. The result for this operation can be:
+   * <ul>
+   * <li>{@link ValueObjectMutationResult#mutated(Object)} if the position was modified. {@link ValueObjectMutationResult#result()} isn't {@code null}.</li>
+   * <li>{@link ValueObjectMutationResult#unchanged()} if the position given is the same, or event is cancelled. {@link ValueObjectMutationResult#result()} is {@code null}.</li>
+   * <li>{@link ValueObjectMutationResult#error()} if the home doesn't exist. {@link ValueObjectMutationResult#result()} is {@code null}.</li>
+   * </ul>
+   * @see io.github.aivruu.homes.home.application.registry.HomeAggregateRootRegistry#findInBoth(String)
+   * @since 1.0.0
+   */
+  public @NotNull ValueObjectMutationResult<@Nullable HomePositionValueObject> updatePosition(
+    final @NotNull Player player,
+    final @NotNull String homeId,
+    final @NotNull Location newLocation
+  ) {
+    final HomeAggregateRoot homeAggregateRoot = this.homeAggregateRootRegistry.findInBoth(homeId);
+    if (homeAggregateRoot == null) {
+      return ValueObjectMutationResult.error();
+    }
+    // Get current home's position (location), and make some checks to avoid unnecessary updates if the
+    // location has no changes.
+    final HomePositionValueObject homePosition = homeAggregateRoot.position();
+    final int x = newLocation.getBlockX();
+    final int y = newLocation.getBlockY();
+    final int z = newLocation.getBlockZ();
+    if (homePosition.x() == x && homePosition.y() == y && homePosition.z() == z) {
+      return ValueObjectMutationResult.unchanged();
+    }
+    // Position-updating event firing process.
+    final HomePositionValueObject newPosition = new HomePositionValueObject(x, y, z);
+    final HomePositionUpdateEvent homePositionUpdateEvent = new HomePositionUpdateEvent(player, homeAggregateRoot, homePosition, newPosition);
+    Bukkit.getPluginManager().callEvent(homePositionUpdateEvent);
+    if (homePositionUpdateEvent.isCancelled()) {
+      return ValueObjectMutationResult.unchanged();
+    }
+    // Update home's position and return the mutation-result.
+    homeAggregateRoot.position(newPosition);
+    return ValueObjectMutationResult.mutated(newPosition);
+  }
+}
