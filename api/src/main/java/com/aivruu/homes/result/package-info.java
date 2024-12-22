37d52793553f948953c46d256db001ca4a9b942f
aivruu <benjaalejandro206@gmail.com>
2024-12-22 12:13:20 -0300

    chore: remove old project code

--- api/src/main/java/com/aivruu/homes/result/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Houses the result for the homes addition/teleport or deletion operations.
- *
- * @since 0.0.1
- */
-package com.aivruu.homes.result;
--- api/src/main/java/com/aivruu/homes/teleport/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Houses the Teleport handling for the saved homes.
- *
- * @since 0.0.1
- */
-package com.aivruu.homes.teleport;
