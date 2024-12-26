1db8b0f9f63a59f2cf180f5da2d84e6143510774
aivruu <benjaalejandro206@gmail.com>
2024-12-25 22:36:20 -0300

    chore(HomesPluginLoader): move to application package

--- plugin/src/main/java/io/github/aivruu/homes/library/application/HomesPluginLoader.java
+++ plugin/src/main/java/io/github/aivruu/homes/library/application/HomesPluginLoader.java
@@ -14,7 +14,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-package io.github.aivruu.homes.library.infrastructure;
+package io.github.aivruu.homes.library.application;
 
 import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
 import io.papermc.paper.plugin.loader.PluginLoader;
