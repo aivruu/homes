8778b145e5283377d9d83248fad3370da7f2f4c2
aivruu <benjaalejandro206@gmail.com>
2024-12-27 22:00:31 -0300

    chore(Homes): add documentation

--- api/src/main/java/io/github/aivruu/homes/api/application/Homes.java
+++ api/src/main/java/io/github/aivruu/homes/api/application/Homes.java
@@ -20,26 +20,68 @@ import io.github.aivruu.homes.aggregate.domain.registry.AggregateRootRegistry;
 import io.github.aivruu.homes.aggregate.domain.repository.AggregateRootRepository;
 import io.github.aivruu.homes.home.application.HomeCreatorService;
 import io.github.aivruu.homes.home.application.HomePositionUpdater;
-import io.github.aivruu.homes.home.domain.HomeAggregateRoot;
 import io.github.aivruu.homes.player.application.PlayerHomeController;
 import io.github.aivruu.homes.player.application.PlayerManagerService;
 import io.github.aivruu.homes.player.domain.PlayerAggregateRoot;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * An interface used for plugin's API classes instances accessing.
+ *
+ * @since 2.0.0
+ */
 public interface Homes {
+  /**
+   * Returns the {@link AggregateRootRepository} implementation for {@link PlayerAggregateRoot}
+   * in-cache management.
+   *
+   * @return The {@link AggregateRootRepository} implementation for {@link PlayerAggregateRoot}.
+   * @since 2.0.0
+   */
   @NotNull AggregateRootRepository<PlayerAggregateRoot> playerCacheRepository();
 
-  @NotNull AggregateRootRepository<HomeAggregateRoot> homeCacheRepository();
-
+  /**
+   * Returns the {@link AggregateRootRegistry} implementation for {@link PlayerAggregateRoot}
+   * global-registry management.
+   *
+   * @return The {@link AggregateRootRegistry} implementation for {@link PlayerAggregateRoot}.
+   * @since 2.0.0
+   */
   @NotNull AggregateRootRegistry<PlayerAggregateRoot> playerRegistry();
 
-  @NotNull AggregateRootRegistry<HomeAggregateRoot> homeRegistry();
-
+  /**
+   * Returns the {@link HomeCreatorService} instance for {@link io.github.aivruu.homes.home.domain.HomeModelEntity} creation
+   * and deletion handling.
+   *
+   * @return The {@link HomeCreatorService}.
+   * @since 2.0.0
+   */
   @NotNull HomeCreatorService homeCreatorService();
 
+  /**
+   * Returns the {@link HomePositionUpdater} instance for {@link io.github.aivruu.homes.home.domain.HomeModelEntity}'s position
+   * modifying.
+   *
+   * @return The {@link HomePositionUpdater}.
+   * @since 2.0.0
+   */
   @NotNull HomePositionUpdater homePositionUpdater();
 
+  /**
+   * Returns the {@link PlayerHomeController} instance for {@link PlayerAggregateRoot}'s homes
+   * management, such as addition, deletion or teleporting.
+   *
+   * @return The {@link PlayerHomeController}.
+   * @since 2.0.0
+   */
   @NotNull PlayerHomeController playerHomeController();
 
+  /**
+   * Returns the {@link PlayerManagerService} instance for {@link PlayerAggregateRoot} loading
+   * and unloading operations.
+   *
+   * @return The {@link PlayerManagerService}.
+   * @since 2.0.0
+   */
   @NotNull PlayerManagerService playerManagerService();
 }
