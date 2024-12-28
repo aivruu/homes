2f2d489e3f0dbba1a521b2be56d74902a59355db
aivruu <benjaalejandro206@gmail.com>
2024-12-27 21:49:07 -0300

    chore(homes): Reduce complexity by removing individual homes aggregate-root and registry/cache management classes

--- api/src/main/java/io/github/aivruu/homes/home/application/registry/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides registry-contract implementation for {@link io.github.aivruu.homes.home.domain.HomeModelEntity}.
- *
- * @since 2.0.0
- */
-package io.github.aivruu.homes.home.application.registry;
